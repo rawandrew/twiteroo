613b67c55f06d52a2abb60751331fd42fd660ea9
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-10 00:12:33 +0200

    Extract single tweet component from the tweet list

--- app/assets/frontend/components/TweetList.jsx
+++ app/assets/frontend/components/TweetList.jsx
@@ -1,23 +1,13 @@
+import Tweet from "./Tweet"
+
 export default class TweetList extends React.Component {
   render() {
     return (
       <div>
         <ul classNme="collection">
-          <li className="collection-item avatar">
-            <i className="material-icons circle">person_pin</i>
-            <span className="title">Andrei Crudu</span>
-            <p>My #FirstTweet</p>
-          </li>
-          <li className="collection-item avatar">
-            <i className="material-icons circle">person_pin</i>
-            <span className="title">Andrei Crudu</span>
-            <p>My #SecondTweet</p>
-          </li>
-          <li className="collection-item avatar">
-            <i className="material-icons circle">person_pin</i>
-            <span className="title">Andrei Crudu</span>
-            <p>My #ThirdTweet</p>
-          </li>
+          <Tweet />
+          <Tweet />
+          <Tweet />
         </ul>
       </div>
     );
--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -162,7 +162,7 @@
 /*!******************************************************!*\
   !*** ./app/assets/frontend/components/TweetList.jsx ***!
   \******************************************************/
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
 	
@@ -171,6 +171,12 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
+	
+	var _Tweet = __webpack_require__(/*! ./Tweet */ 3);
+	
+	var _Tweet2 = _interopRequireDefault(_Tweet);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
@@ -196,63 +202,9 @@
 	        React.createElement(
 	          "ul",
 	          { classNme: "collection" },
-	          React.createElement(
-	            "li",
-	            { className: "collection-item avatar" },
-	            React.createElement(
-	              "i",
-	              { className: "material-icons circle" },
-	              "person_pin"
-	            ),
-	            React.createElement(
-	              "span",
-	              { className: "title" },
-	              "Andrei Crudu"
-	            ),
-	            React.createElement(
-	              "p",
-	              null,
-	              "My #FirstTweet"
-	            )
-	          ),
-	          React.createElement(
-	            "li",
-	            { className: "collection-item avatar" },
-	            React.createElement(
-	              "i",
-	              { className: "material-icons circle" },
-	              "person_pin"
-	            ),
-	            React.createElement(
-	              "span",
-	              { className: "title" },
-	              "Andrei Crudu"
-	            ),
-	            React.createElement(
-	              "p",
-	              null,
-	              "My #SecondTweet"
-	            )
-	          ),
-	          React.createElement(
-	            "li",
-	            { className: "collection-item avatar" },
-	            React.createElement(
-	              "i",
-	              { className: "material-icons circle" },
-	              "person_pin"
-	            ),
-	            React.createElement(
-	              "span",
-	              { className: "title" },
-	              "Andrei Crudu"
-	            ),
-	            React.createElement(
-	              "p",
-	              null,
-	              "My #ThirdTweet"
-	            )
-	          )
+	          React.createElement(_Tweet2.default, null),
+	          React.createElement(_Tweet2.default, null),
+	          React.createElement(_Tweet2.default, null)
 	        )
 	      );
 	    }
@@ -262,6 +214,66 @@
 	})(React.Component);
 	
 	exports.default = TweetList;
+
+/***/ },
+/* 3 */
+/*!**************************************************!*\
+  !*** ./app/assets/frontend/components/Tweet.jsx ***!
+  \**************************************************/
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Tweet = (function (_React$Component) {
+	  _inherits(Tweet, _React$Component);
+	
+	  function Tweet() {
+	    _classCallCheck(this, Tweet);
+	
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Tweet).apply(this, arguments));
+	  }
+	
+	  _createClass(Tweet, [{
+	    key: "render",
+	    value: function render() {
+	      return React.createElement(
+	        "li",
+	        { className: "collection-item avatar" },
+	        React.createElement(
+	          "i",
+	          { className: "material-icons circle" },
+	          "person_pin"
+	        ),
+	        React.createElement(
+	          "span",
+	          { className: "title" },
+	          "Andrei Crudu"
+	        ),
+	        React.createElement(
+	          "p",
+	          null,
+	          "My #FirstTweet"
+	        )
+	      );
+	    }
+	  }]);
+	
+	  return Tweet;
+	})(React.Component);
+	
+	exports.default = Tweet;
 
 /***/ }
 /******/ ]);
