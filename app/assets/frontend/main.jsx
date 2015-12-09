d86cbde21008e24e7c29222ce65a2d42282395b1
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-10 00:24:38 +0200

    Add tweet mock data object and use it to populate the tweets

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,18 @@
 import TweetBox from "./components/TweetBox"
 import TweetList from "./components/TweetList"
 
+let mockTweets = [
+  { id: 1, name: 'Andrei Crudu', body: 'My #FirstTweet' },
+  { id: 2, name: 'Andrei Crudu', body: 'My #SecondTweet' },
+  { id: 3, name: 'Andrei Crudu', body: 'My #ThirdTweet' }
+];
+
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <TweetBox />
-        <TweetList />
+        <TweetList tweets={mockTweets} />
       </div>);
   }
 }
--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -67,6 +67,8 @@
 	
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 	
+	var mockTweets = [{ id: 1, name: 'Andrei Crudu', body: 'My #FirstTweet' }, { id: 2, name: 'Andrei Crudu', body: 'My #SecondTweet' }, { id: 3, name: 'Andrei Crudu', body: 'My #ThirdTweet' }];
+	
 	var Main = (function (_React$Component) {
 	  _inherits(Main, _React$Component);
 	
@@ -83,7 +85,7 @@
 	        "div",
 	        { className: "container" },
 	        React.createElement(_TweetBox2.default, null),
-	        React.createElement(_TweetList2.default, null)
+	        React.createElement(_TweetList2.default, { tweets: mockTweets })
 	      );
 	    }
 	  }]);
@@ -166,6 +168,8 @@
 
 	"use strict";
 	
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
 	Object.defineProperty(exports, "__esModule", {
@@ -196,15 +200,16 @@
 	  _createClass(TweetList, [{
 	    key: "render",
 	    value: function render() {
+	      var tweets = this.props.tweets.map(function (tweet) {
+	        return React.createElement(_Tweet2.default, _extends({ key: tweet.id }, tweet));
+	      });
 	      return React.createElement(
 	        "div",
 	        null,
 	        React.createElement(
 	          "ul",
 	          { classNme: "collection" },
-	          React.createElement(_Tweet2.default, null),
-	          React.createElement(_Tweet2.default, null),
-	          React.createElement(_Tweet2.default, null)
+	          tweets
 	        )
 	      );
 	    }
@@ -259,12 +264,12 @@
 	        React.createElement(
 	          "span",
 	          { className: "title" },
-	          "Andrei Crudu"
+	          this.props.name
 	        ),
 	        React.createElement(
 	          "p",
 	          null,
-	          "My #FirstTweet"
+	          this.props.body
 	        )
 	      );
 	    }
