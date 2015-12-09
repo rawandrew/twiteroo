90523d6df5e426757830d6e70f2d604fee6eed47
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-09 14:55:30 +0200

    Add webpack for automatic module packaging and good code structure

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,18 @@
+import Greet from './greet';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Greet />
+    );
+  }
+}
+
+let documentReady = () => {
+  React.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
