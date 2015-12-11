36f44ab1acf32f29d3a9aef91bb1c336897fdc47
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-11 20:58:24 +0200

    Add the profile controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-
   root 'home#index'
   devise_for :users
+
+  get 'profile' => 'profile#index'
+  post 'profile' => 'profile#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
