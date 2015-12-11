71aae4a677108a24a550bd47b1a4c04ce4d3fdf4
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-11 21:22:10 +0200

    Make the first name and last name updatable through the profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,6 +4,13 @@ class ProfileController < ApplicationController
   end
 
   def update
-
+    current_user.update(user_params)
+    redirect_to root_path
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
 end
