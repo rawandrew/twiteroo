7652c068fa11dfe789394d0d05df55e0e7457c38
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-11 22:41:02 +0200

    Display the time a tweet was tweeted and formate the time with moment js

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -8,19 +8,29 @@ class Main extends React.Component {
     this.state = { tweetsList: [] };
   }
 
+  formattedTweets(tweetsList) {
+    let formattedList = tweetsList.map(tweet => {
+      tweet.formattedDate = moment(tweet.created_at).fromNow();
+      return tweet;
+    });
+    return {
+      tweetsList: formattedList
+    };
+  }
+
   addTweet(tweetToAdd) {
     $.post("/tweets", { tweet: tweetToAdd })
       .success( savedTweet => {
         let newTweetsList = this.state.tweetsList;
         newTweetsList.unshift(savedTweet);
-        this.setState({ tweetsList: newTweetsList });
+        this.setState(this.formattedTweets(newTweetsList));
       })
       .error(error => console.log(error));
   }
 
   componentDidMount() {
     $.ajax("/tweets")
-      .success(data => this.setState({ tweetsList: data }))
+      .success(data => this.setState(this.formattedTweets(data)))
       .error(error => console.log(eror));
   }
 
