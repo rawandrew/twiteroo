f336d1b85c36824f7f835ac5c13d422342a4cce8
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-12 13:15:33 +0200

    Get the basic react router setup going

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -2,14 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Router, Route, Link } from 'react-router'
-import { history } from 'react-history'
+import { history } from 'react-router/lib/HashHistory'
 
 import Index from './components/Index'
+
+class App extends React.Comment {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 let documentReady = () => {
   let reactNode = document.getElementById('react');
   if (reactNode) {
-    ReactDOM.render(<Index />, reactNode);
+    ReactDOM.render(
+      <Router history={history}>
+        <Router component={App}>
+          <Router path="/" component={Index} />
+        </Route>
+      </Route>, reactNode);
   }
 };
 
