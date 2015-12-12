d2d07e48c4813d794dbe025bdf01c7d7d975cf93
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-12 13:00:05 +0200

    Add the react router and history package

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { Router, Route, Link } from 'react-router'
+import { history } from 'react-history'
+
 import Index from './components/Index'
 
 let documentReady = () => {
