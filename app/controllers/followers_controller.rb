b84a9f06936474b079b2fcef51d76464548f82f2
Andrei Crudu <andrei.crudu@yahoo.com>
2015-12-12 12:39:00 +0200

    Finish setting up the basic followers api endpoint

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,14 @@
 class FollowersController < ApplicationController
 
   def random
-    render json: User.where(["id != ?", current_user.id]).all
+    render json: User.where(["id != ?", current_user.id])
+                     .order("random()").all
+  end
+
+  def create
+    follower = Follower.create(user_id: params[:user_id],
+                               followed_by: current_user.id)
+    render json: follower
   end
 
 end
